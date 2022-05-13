28d204fa9e715d12cf89259924b7dcdb8ff04911
vzhd1701 <vzhd1701@gmail.com>
2022-05-13 12:55:32 +0500

    fix: update

--- testmupdf/__main__.py
+++ testmupdf/__main__.py
@@ -11,15 +11,21 @@ def _get_pdf_first_page_png(pdf_bin: bytes):
     return pix.tobytes()
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(
         description="Extract first page of PDF as PNG image."
     )
     parser.add_argument("pdf", type=argparse.FileType("rb"), help="PDF file")
-    parser.add_argument("-o", type=Path, required=True, help="Output PNG")
+    parser.add_argument(
+        "-o", metavar="OUT", type=Path, required=True, help="Output PNG"
+    )
 
     args = parser.parse_args()
 
     png_data = _get_pdf_first_page_png(args.pdf.read())
     if args.o:
         args.o.write_bytes(png_data)
+
+
+if __name__ == "__main__":
+    main()
